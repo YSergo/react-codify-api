752ad9b97777cb10e81fb3abbfb96f148c684857
YSergo <102493873+YSergo@users.noreply.github.com>
2023-09-24 03:47:55 +0400

    Hide SECRET_KEY

--- api_project/settings.py
+++ api_project/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+
+from decouple import config
 import django_heroku
 import dj_database_url
 import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jd-@7*uq5aox2crn5u#024)&3a)e$m-@m8y^ed0ivfb7=fv(ko'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
