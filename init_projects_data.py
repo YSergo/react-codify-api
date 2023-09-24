9f739d22ef0d92b3f62a65e8f86e8fe2ec17db37
YSergo <102493873+YSergo@users.noreply.github.com>
2023-09-24 08:59:07 +0400

    Update projects data

--- init_projects_data.py
+++ init_projects_data.py
@@ -4,31 +4,31 @@ data = [
   {
     "title": "Дирекция",
     "description": "Дизайн и разработка страницы объектов для размещения рекламы.",
-    "link": "https://codify.software/img/projects/direction.jpg",
+    "link": "https://apialisada11-721689c1d185.herokuapp.com/static/img/direction.jpg",
     "url_to_project": "https://directionadv.ru/objects/testovyy-obekt"
   },
   {
     "title": "Giftly",
     "description": "UX/UI дизайн и разработка новой социальной сети для авторов контента.",
-    "link": "https://codify.software/img/projects/giftly.jpg",
+    "link": "https://apialisada11-721689c1d185.herokuapp.com/static/img/giftly.jpg",
     "url_to_project": None
   },
   {
     "title": "SushiMore",
     "description": "Проектирование дизайна мобильного приложения для сервиса доставки ресторана японской кухни.",
-    "link": "https://codify.software/img/projects/sushi.jpg",
+    "link": "https://apialisada11-721689c1d185.herokuapp.com/static/img/sushi.jpg",
     "url_to_project": None
   },
   {
     "title": "Солнце",
     "description": "Разработка в ограниченные сроки сайта для нового детского телеканала «Солнце».",
-    "link": "https://codify.software/img/projects/sun.jpg",
+    "link": "https://apialisada11-721689c1d185.herokuapp.com/static/img/sun.jpg",
     "url_to_project": "https://sun-tv.ru/"
   },
   {
     "title": "Arca Интерьеры",
     "description": "Дизайн и разработка под ключ. ",
-    "link": "https://codify.software/img/projects/arca.jpg",
+    "link": "https://apialisada11-721689c1d185.herokuapp.com/static/img/arca.jpg",
     "url_to_project": "https://arcadesign.ru/"
   }
 ]
