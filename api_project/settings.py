970bd32e10f05921eb5591088979c7801ff0a17a
YSergo <102493873+YSergo@users.noreply.github.com>
2023-09-24 06:37:59 +0400

    Back to sqlite

--- api_project/settings.py
+++ api_project/settings.py
@@ -84,8 +84,11 @@ WSGI_APPLICATION = 'api_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'))
-}
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
+ }
 
 
 # Password validation
