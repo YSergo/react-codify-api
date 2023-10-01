9f698c261e6d59ce689c93da6856cfb1a0efbe31
YSergo <102493873+YSergo@users.noreply.github.com>
2023-10-01 16:37:10 +0400

    Fix init data script

--- init_projects_data.py
+++ init_projects_data.py
@@ -4,31 +4,31 @@ data = [
   {
     "title": "Дирекция",
     "description": "Дизайн и разработка страницы объектов для размещения рекламы.",
-    "image_url": "img/direction.jpg",
+    "image_url": "/react-codify/img/direction.jpg",
     "url_to_project": "https://directionadv.ru/objects/testovyy-obekt"
   },
   {
     "title": "Giftly",
     "description": "UX/UI дизайн и разработка новой социальной сети для авторов контента.",
-    "image_url": "img/giftly.jpg",
+    "image_url": "/react-codify/img/giftly.jpg",
     "url_to_project": None
   },
   {
     "title": "SushiMore",
     "description": "Проектирование дизайна мобильного приложения для сервиса доставки ресторана японской кухни.",
-    "image_url": "img/sushi.jpg",
+    "image_url": "/react-codify/img/sushi.jpg",
     "url_to_project": None
   },
   {
     "title": "Солнце",
     "description": "Разработка в ограниченные сроки сайта для нового детского телеканала «Солнце».",
-    "image_url": "img/sun.jpg",
+    "image_url": "/react-codify/img/sun.jpg",
     "url_to_project": "https://sun-tv.ru/"
   },
   {
     "title": "Arca Интерьеры",
     "description": "Дизайн и разработка под ключ. ",
-    "image_url": "img/arca.jpg",
+    "image_url": "/react-codify/img/arca.jpg",
     "url_to_project": "https://arcadesign.ru/"
   }
 ]
